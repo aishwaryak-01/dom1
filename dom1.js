9d5d1ec34a58dec52c82e6b0354cff15442478aa
aishwaryak-01 <snigdha0119@gmail.com>
2023-06-10 18:29:21 +0530

    dom1 commit

---
+++ dom1.js
@@ -0,0 +1,9 @@
+var item=document.getElementsByClassName('list-group-item');
+console.log(item);
+console.log(item[2]);
+item[2].style.backgroundColor="green";
+
+item[0].style.fontWeight="bold";
+item[1].style.fontWeight="bold";
+item[2].style.fontWeight="bold";
+item[3].style.fontWeight="bold";
